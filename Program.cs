8169dfbe1e66a541355398ec9f6b60f2afba81f7
Gabriella Sampaio <e107ti@etec.sp.gov.br>
2025-03-17 20:37:41 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -1,66 +1,42 @@
 ﻿
 Console.Clear();
 
-String especiePet= "GATO";
-String racaPet= "frajolaaaaaaa";
-String nomePet= "charlie";
-String idadePet= "3";
-String pelagemPet= "frajola";
-String nomeTutor= "Gabi";
-String contatoTutor= "13991";
+String especiePet;
+String racaPet;
+String nomePet;
+String idadePet;
+String pelagemPet;
+String nomeTutor;
+String contatoTutor;
 
-/*
 Console.ForegroundColor = ConsoleColor.Magenta;
-Console.WriteLine("        💖 Bem vindo 💖         "); Thread.Sleep(1000);
+Console.WriteLine("          💖 Bem vindo 💖           "); Thread.Sleep(1000);
 Console.BackgroundColor = ConsoleColor.White;
 Console.ForegroundColor = ConsoleColor.DarkMagenta;
 Console.WriteLine("            CADASTRO             ");
 Console.ForegroundColor = ConsoleColor.Black;
-Console.WriteLine("🐾🌤️ Pet Hotel Raio de Sol 🌤️🐾 "); Thread.Sleep(1300);
+Console.WriteLine("🐾 🌤️  Pet Hotel Raio de Sol  🌤️ 🐾 "); Thread.Sleep(1300);
 Console.ResetColor();
 
 Console.WriteLine();
 Console.WriteLine("Digite os dados solicitados: "); Thread.Sleep(500);
 Console.WriteLine();
 
-Console.Write("☀️ Espécie         : "); especiePet= Console.ReadLine()!;
-Console.Write("☀️ Raça            : "); racaPet= Console.ReadLine()!;
-Console.Write("☀️ Nome do pet     : "); nomePet= Console.ReadLine()!;
-Console.Write("☀️ Idade           : "); idadePet= Console.ReadLine()!;
-Console.Write("☀️ Pelagem/cor     : "); pelagemPet= Console.ReadLine()!;
-Console.Write("☀️ Tutor (Seu nome): "); nomeTutor= Console.ReadLine()!;
-Console.Write("☀️ Telefone        : "); contatoTutor= Console.ReadLine()!;
+Console.Write("☀️  Espécie         : "); especiePet= Console.ReadLine()!;
+Console.Write("☀️  Raça            : "); racaPet= Console.ReadLine()!;
+Console.Write("☀️  Nome do pet     : "); nomePet= Console.ReadLine()!;
+Console.Write("☀️  Idade           : "); idadePet= Console.ReadLine()!;
+Console.Write("☀️  Pelagem/cor     : "); pelagemPet= Console.ReadLine()!;
+Console.Write("☀️  Tutor (Seu nome): "); nomeTutor= Console.ReadLine()!;
+Console.Write("☀️  Telefone        : "); contatoTutor= Console.ReadLine()!;
 
 Console.WriteLine();
 Console.ForegroundColor = ConsoleColor.Magenta;
-Console.WriteLine("Obrigada 💕"); Thread.Sleep(1000);
+Console.WriteLine("Obrigada 💕"); Thread.Sleep(500);
+Console.WriteLine("Aqui está sua ficha:"); Thread.Sleep(1500);
 Console.ResetColor();
-
-Console.WriteLine(@$"
-+=========================================================+
-|                 Pet Hotel ""Nem um pio""                  |
-+=========================================================+
-| Espécie:{especiePet.PadLeft(10, '.')} | Raça:  |
-+=========================================================+
-| Atende pela alcunha de: ...........................ZECA |
-| Idade: 03 ano(s) | Pelagem/cor: ...............CARAMELO |
-+=========================================================+");
-*/
-
 Console.Clear();
-Console.WriteLine(@$"
-🟪〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️➕
- |              🌤️   Pet Hotel Raio de Sol 🌤️                |
-➕➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➕
- | Espécie:{especiePet.PadLeft(16, '.').ToUpper()} | Raça: ............INDEFINIDA |
-⬜➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➕
-⬜ Nome do pet:.......................................ZECA |
-⬜ Idade: 03 ano(s) | Pelagem/cor: ...............CARAMELO |
-🔲➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➕
 
-💘💘💘💘💘
-,,,,,,,,,,
-");
 
 
 Console.BackgroundColor = ConsoleColor.White;
@@ -73,7 +49,7 @@ Console.Write(" Pet Hotel Raio de Sol");
 
 Console.ForegroundColor = ConsoleColor.Black;
 Console.WriteLine(" 🌤️               | ");
-Console.WriteLine(" |               🌤️    Ficha de inscrição                   | ");
+Console.WriteLine(" |               🌤️     Ficha de cadastro                   | ");
 
 Console.WriteLine("➕〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️〰️➕ ");
 
@@ -81,10 +57,37 @@ Console.ResetColor();
 Console.BackgroundColor = ConsoleColor.White;
 Console.ForegroundColor = ConsoleColor.Black;
 Console.WriteLine(" |➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖ | ");
-Console.WriteLine($" | Espécie: {especiePet.PadRight(15, ' ').ToUpper()}| Raça: {racaPet.PadRight(23, ' ').ToUpper()} | ");
+Console.WriteLine($" | Espécie: {especiePet
+    .PadRight(15, ' ')
+    .ToUpper()
+    .Substring(0, 15)}| Raça: {racaPet
+    .PadRight(23, ' ')
+    .ToUpper()
+    .Substring(0, 23)} | ");
 Console.WriteLine(" |➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖ | ");
-Console.WriteLine($" | Nome do Pet: {nomePet.PadRight(43, ' ').ToUpper()}| ");
+Console.WriteLine($" | Nome do Pet: {nomePet
+    .PadRight(43, ' ')
+    .ToUpper()
+    .Substring(0, 43)}| ");
 Console.WriteLine(" |➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖ | ");
+Console.WriteLine($" | Idade do Pet: {idadePet
+    .PadRight(10, ' ')
+    .ToUpper()
+    .Substring(0, 10)}| Pelagem (cor): {pelagemPet
+    .PadRight(14, ' ')
+    .ToUpper()
+    .Substring(0, 14)} | ");
+Console.WriteLine(" |➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖ | ");
+Console.WriteLine($" | Nome do tutor: {nomeTutor
+    .PadRight(41, ' ')
+    .ToUpper()
+    .Substring(0, 41)}| ");
+Console.WriteLine($" | Contato: {contatoTutor
+    .PadRight(47, ' ')
+    .ToUpper()
+    .Substring(0, 47)}| ");
+Console.WriteLine("➕➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➕ ");
+
 
 Console.ResetColor();
 
